bccfa0e4930ceb36bd02a599853c30c75f131dfd
Anupam Tiwari <lonesamurai1603@gmail.com>
2024-07-14 12:16:28 +0530

    Completed routing functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import AllBooks from "./pages/AllBooks";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Cart from "./pages/Cart";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -11,6 +16,12 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route  path="/all-books" element={<AllBooks />} />
+          <Route  path="/Signin" element={<Signin />} />
+          <Route  path="/Signup" element={<Signup />} />
+          <Route  path="/cart" element={<Cart />} />
+          <Route  path="/profile" element={<Profile />} />
+          
         </Routes>
         <Footer />
       </Router>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const links = [
@@ -22,32 +23,37 @@ const Navbar = () => {
   ];
   return (
     <div className=" flex items-center  justify-between bg-zinc-800 text-white px-8 py-2">
-      <div className="flex items-center">
+      <Link to="/" className="flex items-center">
         <img
           className="h-10 me-4"
           src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png"
           alt="logo"
         />
         <h1 className="text-2xl font-semibold">BookHeaven</h1>
-      </div>
+      </Link>
       <div className="nav-links-bookheaven flex items-center gap-4 ">
         <div className=" flex gap-4">
           {links.map((items, i) => (
-            <div
+            <Link
+              to={items.link}
               className="hover:text-blue-500 transition-all duration-300"
               key={i}
             >
               {items.title}
-            </div>
+            </Link>
           ))}
         </div>
         <div className="flex gap-4">
-           <button className="px-4 py-1   border rounded border-blue-500  hover:bg-white hover:text-zinc-800 transition-all transition-duration-300">
+           <Link 
+            to="/Signin"
+           className="px-4 py-1   border rounded border-blue-500  hover:bg-white hover:text-zinc-800 transition-all transition-duration-300">
             SignIn
-            </button>
-           <button className="px-4 py-1  bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all transition-duration-300 ">
+            </Link>
+           <Link 
+           to="/Signup"
+           className="px-4 py-1  bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all transition-duration-300 ">
             SignUp
-            </button>
+            </Link>
         </div>
       </div>
     </div>
